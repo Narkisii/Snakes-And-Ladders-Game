8ed127d5f9507dfed20fad44aeb66b72c2f59d5a
Itay <itay.olivcovitz@gmail.com>
2024-01-09 16:27:56 +0200

    Commit example

--- Project/src/control/Class_Example_Controll.java
+++ Project/src/control/Class_Example_Controll.java
@@ -1,5 +1,9 @@
 package control;
 
 public class Class_Example_Controll {
-
+    public static void main(String[] args) {
+        System.out.println("Hello,i change that code ");
+        System.out.println("and i finished for today ");
+        System.out.println("dont forget to click cntrl s - for saving");
+    }
 }
