a6cb958dcfc6e801c57876ef74f9237d499bf7b8
Ariel <arielbubis@gmail.com>
2024-02-01 18:57:28 +0200

    Css, Board and menu

--- Project/src/view/MenuScreen.java
+++ Project/src/view/MenuScreen.java
@@ -4,17 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-public class Board extends Application {
-
-
+public class MenuScreen extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
-		Parent root = FXMLLoader.load(getClass().getResource("Board.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("MenuScreen.fxml"));
 		Scene scene = new Scene (root);
 		stage.setScene(scene);
 		stage.show();
@@ -23,5 +19,4 @@ public class Board extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 }
