e434028f859e7d5b5fbd9d82e5f4f6e0e68412f8
ArielBubis <arielbubis@gmail.com>
2024-03-01 13:02:03 +0200

    cpuplayer minor fix

--- Project/src/model/cpu_Player.java
+++ Project/src/model/cpu_Player.java
@@ -10,24 +10,28 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.util.Duration;
 
-interface Command {
-	void execute();
-	void execute_question();
-}
+//interface Command {
+//	void execute();
+//	void execute_question();
+//}
 
 public class cpu_Player extends Player {
 	int diceResult;
 	Board board;
+	String difficulty;
 	private questionPopControl question_Controll;
+	private BoardControl board_Controll;
 	public cpu_Player(int ID, String color, String name, String token) {
 		super(ID, color, name, token);
+		difficulty = GameData.getInstance().getDifficulty();
 		System.out.println(this.toString());
 	}
 
-	public void execute() {
+	public void executeDice_roll() {
 		// Simulate rolling the dice and store the result
-		this.board  = GameData.getInstance().getBoard();
-		diceResult = board.get_Dice_Result();		
+//		this.board  = GameData.getInstance().getBoard();
+		diceResult = board.get_Dice_Result();	
+		board_Controll.roll(diceResult, this);
 	}
 
 	public void set_question_controll(questionPopControl questionPopControl) {
@@ -35,6 +39,11 @@ public class cpu_Player extends Player {
 //		question_Controll.getAnswerFour().setSelected(false);
 	}
 	
+	public void set_board_controll(BoardControl questionPopControl) {
+		board_Controll = questionPopControl;
+//		question_Controll.getAnswerFour().setSelected(false);
+	}
+
     public void selectAnswer() {
         ArrayList<RadioButton> answers = new ArrayList<RadioButton>();
         RadioButton answ1 = question_Controll.getAnswerOne();
